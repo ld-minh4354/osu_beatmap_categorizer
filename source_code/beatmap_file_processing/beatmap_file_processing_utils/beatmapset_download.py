5bdee5bb2e874b2a3f729f03c3192ac07a4e55b6
Minh <182339j@student.hci.edu.sg>
2024-04-30 00:27:55 +0700

    modify beatmapset_download

--- source_code/beatmap_file_processing/beatmap_file_processing_utils/beatmapset_download.py
+++ source_code/beatmap_file_processing/beatmap_file_processing_utils/beatmapset_download.py
@@ -7,14 +7,19 @@ def download_beatmapset(beatmapset_id):
     time.sleep(0.5)
 
     # request beatmapset from website
-    req = requests.get(f"https://beatconnect.io/b/{beatmapset_id}")
+    req = requests.get(f'https://beatconnect.io/b/{beatmapset_id}')
 
-    # save file
-    f = open(os.path.join(ROOT_SRC_DIR, os.pardir, 'data', 'interim', f'{beatmapset_id}.zip'), 'wb')
-    f.write(req.content)
-    f.close()
-    print(f"Download of beatmapset ID {beatmapset_id} successful.")
+    # return file content
+    return req.content 
 
 
 if __name__ == '__main__':
-    download_beatmapset(1853618)
+    # call function
+    f_content = download_beatmapset(1853618)
+    print('Function finished running.')
+
+    # save file
+    f = open(os.path.join(ROOT_SRC_DIR, os.pardir, 'data', 'interim', '1853618.zip'), 'wb')
+    f.write(f_content)
+    f.close()
+    print('Download of beatmapset ID 1853618 successful.')
