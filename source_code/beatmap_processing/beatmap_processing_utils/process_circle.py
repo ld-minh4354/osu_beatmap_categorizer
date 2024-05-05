ba0e22418987b44a822db99e670090bc4dbfd1fa
Minh <182339j@student.hci.edu.sg>
2024-05-06 04:04:53 +0700

    push process_circle.py

--- source_code/beatmap_processing/beatmap_processing_utils/process_circle.py
+++ source_code/beatmap_processing/beatmap_processing_utils/process_circle.py
@@ -1,2 +1,8 @@
 def process_circle(hit_object):
-    pass
+    x, y, time, _, _, _ = hit_object.split(',')
+
+    return [int(time), int(x), int(y), 0]
+
+
+if __name__ == '__main__':
+    print(process_circle('353,150,11662,1,8,0:3:0:0:'))
